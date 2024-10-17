c7f18752783cf5d159b458b7f82374dd4112ac2f
william-le1004 <kienle4456@gmail.com>
2024-10-17 21:01:11 +0700

    Add missing file

---
+++ melodic-api/Api/Controller/AuthController.cs
@@ -0,0 +1,31 @@
+using Application.Identity;
+using Application.Models.Identity;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controller
+{
+    [Route("api/authenticate")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<AuthResponse>> Login(AuthRequest request)
+        {
+            return Ok(await _authService.Login(request));
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<RegistrationResponse>> Register(RegistrationRequest request)
+        {
+            return Ok(await _authService.Register(request));
+        }
+    }
+}
