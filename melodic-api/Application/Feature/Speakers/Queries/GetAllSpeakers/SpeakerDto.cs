d782e4355187dc83b2e2c42c2e85ff736c6961f7
ya3k <truongdinhvan1233@gmail.com>
2024-10-17 22:58:51 +0700

    list speaker

---
+++ melodic-api/Application/Feature/Speakers/Queries/GetAllSpeakers/SpeakerDto.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Feature.Speakers.Queries.GetAllSpeakers
+{
+    public class SpeakerDto
+    {
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public Guid BrandId { get; set; }
+        //brand
+        public double Price { get; set; }      
+        public string? Decription { get; set; }     
+        public int? UnitInStock { get; set; }
+        public string? Img { get; set; }
+    }
+}
