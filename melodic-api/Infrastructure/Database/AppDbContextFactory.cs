96e934e99a1b3397bfd88b351487dce1cc44f4d8
kh0abug <kd031212@gmail.com>
2024-11-06 10:15:52 +0700

    CCC-RegisterDbContext

--- melodic-api/Infrastructure/Database/AppDbContextFactory.cs
+++ melodic-api/Infrastructure/Database/AppDbContextFactory.cs
@@ -8,7 +8,7 @@ public class AppDbContextFactory : IDesignTimeDbContextFactory<MelodicDbContext>
     public MelodicDbContext CreateDbContext(string[] args)
     {
         var optionsBuilder = new DbContextOptionsBuilder<MelodicDbContext>();
-        optionsBuilder.UseSqlServer("Data Source=.;User Id=sa; Password=123;Initial Catalog=Melodic;Integrated Security=True;Encrypt=False;Trusted_Connection=False");
+        optionsBuilder.UseSqlServer("Data Source=SQL5113.site4now.net;Initial Catalog=db_aae5f9_melodic;User Id=db_aae5f9_melodic_admin;Password=melodic123321");
 
         // Create a mock or default implementation of IUserService
         var userService = new DefaultUserService();
